fe7a54e5a353c5ec1dc320c2359561e8cce2fdf1
Noe0131 <KTC2A31G0017@edu.kyoto-tech.ac.jp>
2025-04-28 10:53:00 +0900

    完成

--- app/_components/bookmark/bookmark.tsx
+++ app/_components/bookmark/bookmark.tsx
@@ -1,46 +1,36 @@
-import Image from "next/image"
+import styles from "./index.module.css";
 
-type plan = {
-    id: number;
-    time: string;
-    name: string;
+type Plan = {
+  id: number;
+  time: string;
+  name: string;
 };
 
-const plans: plan[] = [
-    {id: 2, time: "7:20", name: "出発"},
-    {id: 2, time: "7:37", name: "椥辻"},
-    {id: 2, time: "9:04", name: "夢洲到着"},
-    {id: 2, time: "10:00", name: "東ゲート"},
-    {id: 3, time: "11:00", name: "サウジアラビアパビリオン"},
-    {id: 1, time: "12:00", name: "昼ご飯"},
-    {id: 4, time: "13:00", name: "アメリカ、フランス"},
-    {id: 1, time: "18:00", name: "夜ごはん"},
-    {id: 3, time: "19:30", name: "チェコパビリオン"},
-    {id: 4, time: "20:00", name: "水のショー"},
-    {id: 3, time: "20:30", name: "オーストラリア"},
-    {id: 2, time: "21:30", name: "西ゲートバス"},
+const plans: Plan[] = [
+  { id: 2, time: "7:20", name: "出発" },
+  { id: 2, time: "7:37", name: "椥辻" },
+  { id: 2, time: "9:04", name: "夢洲到着" },
+  { id: 2, time: "10:00", name: "東ゲート" },
+  { id: 3, time: "11:00", name: "サウジアラビアパビリオン" },
+  { id: 1, time: "12:00", name: "昼ご飯" },
+  { id: 4, time: "13:00", name: "アメリカ、フランス" },
+  { id: 1, time: "18:00", name: "夜ごはん" },
+  { id: 3, time: "19:30", name: "チェコパビリオン" },
+  { id: 4, time: "20:00", name: "水のショー" },
+  { id: 3, time: "20:30", name: "オーストラリア" },
+  { id: 2, time: "21:30", name: "西ゲートバス" },
 ];
 
-const bgColors: { [ key: number]: string} = {
-    1: "bg-red-300",
-    2: "bg-blue-300",
-    3: "bg-green-300",
-    4: "bg-yellow-300",
-};
-
-export default function bookmark() {
-    return (
-        <div>
-            {plans.map((plan) => (
-                <div 
-                key={plan.id}
-                className={`flex p-4 m-2 rounded-lg  ${bgColors[plan.id]}`}
-                >
-                <h2 className={"mr-10 text-2xl"}>{plan.time}</h2>
-                <p className={"text-2xl"}>{plan.name}</p>
-                </div>
-            ))}
+export default function Bookmark() {
+  return (
+    <div className={styles.container}>
+      {plans.map((plan, index) => (
+        <div key={index}>
+          <h1 className={`${styles.bookmark} ${styles[`id${plan.id}`]}`}>
+            {plan.time} {plan.name}
+          </h1>
         </div>
-    );
-};
-
+      ))}
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import Bookmark from "@/app/_components/bookmark/bookmark";
-import Image from "next/image"
+import Image from "next/image";
+import "@/styles/styles.css";  
 
 export default function Home() {
   return (
-    <div className="container mx-auto max-w-[1200px]">
-      <h1 className="flex items-center justify-center text-5xl font-bold">大阪万博</h1>
+    <div className="container">
+      <h1 className="title">大阪万博</h1>
       <Bookmark />
       <Image
-      className={""}
-      src="/images/myakumyaku.jpg"
-      alt=""
-      width={800}
-      height={800}
+        className="image"
+        src="/images/myakumyaku.jpg"
+        alt="みゃくみゃく"
+        width={800}
+        height={800}
       />
     </div>
   );
